fa66d8c31d1498fd5aa90a438bc2c69935a36b80
Igor Storchevoy <hellsgor@gmail.com>
2025-09-15 23:51:10 +0400

    Feat[shared/lib]: created getRems function for convert px to rem

--- src/shared/config/styles/typography.ts
+++ src/shared/config/styles/typography.ts
@@ -1,9 +1,10 @@
+import { getRems } from '@/shared/lib';
 import { css } from 'styled-components';
 
 export const typography = css`
   body {
     font-family: PT-sans, sans-serif;
-    font-size: 20px;
+    font-size: ${getRems(20)};
     font-weight: 400;
     font-style: normal;
     line-height: 1.5;
