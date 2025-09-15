36e9658bb4267a32428984ba46e1f5a70929d1a1
Igor Storchevoy <hellsgor@gmail.com>
2025-09-15 22:40:31 +0400

    Feat[shared/styles]: added reset styles

--- src/shared/config/styles/global-styles.ts
+++ src/shared/config/styles/global-styles.ts
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 import normalize from 'styled-normalize';
+import { reset } from './reset';
 
 export const GlobalStyles = createGlobalStyle`
   ${normalize}
+  ${reset}
 `;
