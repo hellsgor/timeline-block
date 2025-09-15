3434c1327b51e11c63407c5ed1ec04c7df3e0d7f
Igor Storchevoy <hellsgor@gmail.com>
2025-09-15 23:39:35 +0400

    Feat[shared/styles]: added basic typography

--- src/shared/config/styles/global-styles.ts
+++ src/shared/config/styles/global-styles.ts
@@ -2,9 +2,11 @@ import { createGlobalStyle } from 'styled-components';
 import normalize from 'styled-normalize';
 import { reset } from './reset';
 import { fonts } from './fonts';
+import { typography } from './typography';
 
 export const GlobalStyles = createGlobalStyle`
   ${normalize}
   ${reset}
   ${fonts}
+  ${typography}
 `;
---
+++ src/shared/config/styles/typography.ts
@@ -0,0 +1,11 @@
+import { css } from 'styled-components';
+
+export const typography = css`
+  body {
+    font-family: PT-sans, sans-serif;
+    font-size: 20px;
+    font-weight: 400;
+    font-style: normal;
+    line-height: 1.5;
+  }
+`;
