d0922e9d3808d88dd8ca5ff436a9ecce542e4e3f
Igor Storchevoy <hellsgor@gmail.com>
2025-09-16 23:24:12 +0400

    Refactor[fonts]: extracted common font rules into a variable for consistency

--- src/shared/config/styles/fonts.ts
+++ src/shared/config/styles/fonts.ts
@@ -1,27 +1,29 @@
 import { css } from 'styled-components';
 
+const fontRules = css`
+  font-style: normal;
+  font-display: swap;
+`;
+
 export const fonts = css`
   @font-face {
     font-family: PT-Sans;
     font-weight: 400;
-    font-style: normal;
-    font-display: swap;
     src: url('/fonts/PTSans-Regular.woff2') format('woff2');
+    ${fontRules}
   }
 
   @font-face {
     font-family: PT-Sans;
     font-weight: 700;
-    font-style: normal;
-    font-display: swap;
     src: url('/fonts/PTSans-Bold.woff2') format('woff2');
+    ${fontRules}
   }
 
   @font-face {
     font-family: Bebas;
     font-weight: 400;
-    font-style: normal;
-    font-display: swap;
     src: url('/fonts/BebasNeue-Regular.woff2') format('woff2');
+    ${fontRules}
   }
 `;
