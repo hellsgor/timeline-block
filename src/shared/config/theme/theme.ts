347926755b66172dcd5f26f152320b9de4301ef0
Igor Storchevoy <hellsgor@gmail.com>
2025-09-16 15:58:20 +0400

    Feat[shared/config/theme]: created safe-indents constant

--- src/shared/config/theme/theme.ts
+++ src/shared/config/theme/theme.ts
@@ -1,11 +1,13 @@
 import { MEDIA } from './media';
 import { PALETTE } from './palette';
+import { SAFE_INDENTS } from './safe-indents';
 import { TRANSITION_PARAMS } from './transitions';
 
 export const THEME = {
   colors: PALETTE,
   media: MEDIA,
   transitions: TRANSITION_PARAMS,
+  safeIndents: SAFE_INDENTS,
 } as const;
 
 export type AppTheme = typeof THEME;
