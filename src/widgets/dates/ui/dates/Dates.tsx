aac9ebe8a5f7c583cc40d97ef8bb9192273d1d75
Igor Storchevoy <hellsgor@gmail.com>
2025-09-17 02:00:07 +0400

    Feat[widgets/dates]: integrate CategorySlider into Dates component and manage active category state

--- src/widgets/dates/ui/dates/Dates.tsx
+++ src/widgets/dates/ui/dates/Dates.tsx
@@ -1,11 +1,22 @@
 import { BackgroundLines, Container, Heading } from '@/shared/ui';
 import * as S from './Dates.styled';
+import type { ITimelineData } from '../../model';
+import { TIMELINE_DATA } from '../../config';
+import 'swiper/swiper-bundle.css';
+import { useState } from 'react';
+import { CategorySlider } from '../category-slider';
 
 type DatesProps = {
   sectionName?: string;
 };
 
 export function Dates({ sectionName = 'dates' }: DatesProps) {
+  const data: ITimelineData = TIMELINE_DATA;
+
+  const [activeCategorySlide] = useState(0);
+
+  const activeCategory = Object.entries(data)[activeCategorySlide];
+
   return (
     <section data-section-name={sectionName}>
       <Container>
@@ -16,6 +27,7 @@ export function Dates({ sectionName = 'dates' }: DatesProps) {
             <br />
             даты
           </Heading>
+          <CategorySlider data={activeCategory[1]} />
         </S.StyledWrapper>
       </Container>
     </section>
