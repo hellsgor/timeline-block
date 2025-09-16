c890f6fdeff4b26eccc30a3c7bb8e15aa1b78c4e
Igor Storchevoy <hellsgor@gmail.com>
2025-09-16 16:19:29 +0400

    Feat[shared/ui/background-lines]: created component

---
+++ src/shared/ui/background-lines/index.ts
@@ -0,0 +1,1 @@
+export { BackgroundLines } from './BackgroundLines';
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,3 +2,4 @@ export { Container } from './container';
 export { Heading } from './heading';
 export { ButtonRounded } from './button-rounded';
 export { Icon } from './icon';
+export { BackgroundLines } from './background-lines';
