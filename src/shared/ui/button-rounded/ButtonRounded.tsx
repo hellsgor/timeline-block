df2d4aac4cee296270b2fd745ff60a8662f08470
Igor Storchevoy <hellsgor@gmail.com>
2025-09-19 14:56:05 +0400

    Update[shared/ui/ButtonRounded]: removed hint prop

--- src/shared/ui/button-rounded/ButtonRounded.tsx
+++ src/shared/ui/button-rounded/ButtonRounded.tsx
@@ -6,7 +6,6 @@ export function ButtonRounded({
   isBordered,
   isShadowed,
   isTransparent,
-  hint,
   ...rest
 }: ButtonRoundedProps) {
   return (
@@ -14,7 +13,6 @@ export function ButtonRounded({
       $isBordered={isBordered}
       $isShadowed={isShadowed}
       $isTransparent={isTransparent}
-      $hint={hint}
       {...rest}
     >
       {children}
